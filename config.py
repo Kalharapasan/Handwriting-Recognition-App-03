e7fc02c0aeb281cc6d91190a5d5940477cb721fe
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-14 22:15:58 +0530

    Paths

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ import os
 from datetime import datetime
 class Config:
     DATABASE_URL = 'sqlite:///db.db'
+    MODEL_PATH = 'models/handwriting_model.h5'
+    UPLOAD_FOLDER = 'data/uploaded'
+    CUSTOM_DATASET_PATH = 'data/custom_dataset'
+    STATIC_FOLDER = 'static'
