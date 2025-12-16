a24c3e5a4710826fb23e215ec6fc0dbd369122e6
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-16 15:07:24 +0530

    Rotate image

--- utils.py
+++ utils.py
@@ -120,5 +120,13 @@ class AdvancedImagePreprocessor:
             angle = -(90 + angle)
         else:
             angle = -angle
+        
+        (h, w) = image.shape[:2]
+        center = (w // 2, h // 2)
+        M = cv2.getRotationMatrix2D(center, angle, 1.0)
+        rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, 
+                               borderMode=cv2.BORDER_REPLICATE)
+        
+        return rotated
 
         
