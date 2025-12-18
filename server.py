6b0ca5a3519e86dcab5e7df2e26308c0324cbb15
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-12-18 11:16:58 +0530

    Setup logging

--- server.py
+++ server.py
@@ -6,3 +6,9 @@ import logging
 from pathlib import Path
 
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
